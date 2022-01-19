70e2fec6834ffad091a8274c68b5a343fa060c06
imizao <2313466338@qq.com>
2022-01-19 18:35:56 +0800

    根据js，使用rust实现

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
-fn large_n<T: std::cmp::PartialOrd>(a:T,b:T) ->T {
-    if a > b {
-        a
-    }else {
-        b
-    }
+// let staff = [
+//   { name: "George", money: 0 },
+//   { name: "Lea", money: 500000 },
+// ];
+// let salary = 1000;
+// staff.forEach((employee) => {
+//   employee.money += salary;
+// });
+#[derive(Debug)]
+pub struct ContextArray {
+   pub name: String,
+   pub money: i32,
 }
 
 fn main() {
-    let a_num = 10;
-    let b_num = 20;
-    let p_num = large_n(a_num,b_num);
-    println!("{}",p_num);
+    let mut statff= vec![
+        ContextArray { name: "George".to_string(), money: 0},
+        ContextArray { name: "Lea".to_string(), money: 1000}
+    ];
+    let salary = 1000;
+
+    statff.iter_mut().for_each(
+        |item| item.money += salary
+    );
+    println!("{:?}", statff)
 }
