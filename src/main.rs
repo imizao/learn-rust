16a855e6b1318ddb9045d185b555cae54062c218
imizao <2313466338@qq.com>
2022-01-25 15:36:00 +0800

    debug

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,13 @@
-// let staff = [
-//   { name: "George", money: 0 },
-//   { name: "Lea", money: 500000 },
-// ];
-// let salary = 1000;
-// staff.forEach((employee) => {
-//   employee.money += salary;
-// });
 #[derive(Debug)]
-pub struct ContextArray {
-   pub name: String,
-   pub money: i32,
+struct Person<'a> {
+    name: &'a str,
+    age: u8
 }
 
 fn main() {
-    let mut statff= vec![
-        ContextArray { name: "George".to_string(), money: 0},
-        ContextArray { name: "Lea".to_string(), money: 1000}
-    ];
-    let salary = 1000;
+    let name = "Peter";
+    let age = 27;
+    let peter = Person { name, age };
 
-    statff.iter_mut().for_each(
-        |item| item.money += salary
-    );
-    println!("{:?}", statff)
+    println!("{:#?}", peter);
 }
